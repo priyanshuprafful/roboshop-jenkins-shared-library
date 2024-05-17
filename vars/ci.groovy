743c099b4e87110a7a7ff56c876bae08ec5e6567
priyanshuprafful <priyanshuprafful@gmail.com>
2024-05-17 15:02:49 +0530

    updating ./roboshop-jenkins-shared-library

---
+++ vars/ci.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Compile/Build') {
+            steps {
+                echo "Compile/Build"
+            }
+
+        }
+        stage('Test Cases'){
+            steps {
+                echo "Test Cases"
+            }
+        }
+
+    }
+}
