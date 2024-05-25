6a2f80ef232f2bc7cfce5b4aaf41d6bb46d723cd
priyanshuprafful <priyanshuprafful@gmail.com>
2024-05-25 14:55:30 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -20,6 +20,6 @@ def testcases() {
 
 def codequality() {
 
-    sh 'sonar-scanner -Dsonar.host.url=http://172.31.38.198:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}'
+    sh 'sonar-scanner -Dsonar.host.url=http://172.31.38.198:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
 
 }
