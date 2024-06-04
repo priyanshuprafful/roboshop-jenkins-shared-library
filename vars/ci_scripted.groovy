dfa3baf8754374c219cf503e3c5ea5ba85099f0d
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-04 05:44:35 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -2,6 +2,10 @@ def call() {
     if (!env.sonar_extra_opts) {
         env.sonar_extra_opts = ""
     }
+
+    if (env.TAG_NAME ==~ ".*"){
+        env.GTAG = "true"
+    }
     node('workstation') {
 
 
@@ -24,10 +28,13 @@ def call() {
                 }
 
             }
+            if (env.GTAG != "true"){
+                stage('Test Cases') {
+                    common.testcases()
+                }
 
-            stage('Test Cases') {
-                common.testcases()
             }
+
 
             stage('Code Quality') {
                 common.codequality()
