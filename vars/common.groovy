17cccc873ee63440aa95ee83fd76d3f32ee03178
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-09 17:24:26 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,7 +35,7 @@ def prepareArtifacts() {
 //    }
 
     if (app_language == "maven") {
-        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+        sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION"
     } else {
         sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
     }
