e8bdc534c77fb8a1d8815fe3e8dee6466c2d18f1
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-11 05:22:39 +0530

    updating ./roboshop-jenkins-shared-library

---
+++ vars/deploy.groovy
@@ -0,0 +1,43 @@
+def call() {
+    pipeline {
+        agent any
+
+        parameters {
+            string(name: 'app_version', defaultValue: '' , description: 'App_Version')
+            string(name: 'component', defaultValue: '' , description: 'Component')
+            string(name: 'environment', defaultValue: '' , description: 'Environment')
+
+
+        }
+
+        stages {
+
+            stage('Update Parameter Store') {
+                steps {
+
+                    sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}"  --overwrite'
+
+                }
+            }
+
+
+
+            stage('Deploy Servers') {
+                steps {
+
+                    sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component-${environment}}" --query "Reservations[*].Instances[*].PrivateipAddress" --output text >/tmp/servers'
+
+                    sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment}'
+
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
