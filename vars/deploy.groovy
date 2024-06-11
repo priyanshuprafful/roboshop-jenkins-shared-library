cd52caa12fc9dffdbae2dca268c5186972738e99
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-11 06:07:24 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -25,24 +25,21 @@ def call() {
             stage('Deploy Servers') {
                 steps {
                     script {
-                        env.NEXUS_USER = sh ( script: "aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
-                        env.NEXUS_PASS = sh ( script: "aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
-                        wrap([$class: 'MaskPasswordsBuildWrapper',
-                              varPasswordPairs: [[password: NEXUS_PASS] , [password:NEXUS_USER]]]) {
 
-                            //   if (app_language == "node_js" || app_language == "angular") {
-                            sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                            // }
+                        env.SSH_PASSWORD = sh ( script: "aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
+                        wrap([$class: 'MaskPasswordsBuildWrapper',
+                              varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+
+                            sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+
+                            sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
+
 
                         }
-                        //   if (app_language == "node_js" || app_language == "angular") {
-                        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                        // }
+
                     }
 
-                    sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
 
-                    sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
 
                 }
             }
