525a55d420f11ba94937e0420e6e477990026640
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-25 05:21:43 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -41,7 +41,7 @@ def prepareArtifacts() {
 //    } else {
 //        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
 //    }
-    docker build -t 529080510656.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .
+   sh 'docker build -t 529080510656.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
 
 
 }
