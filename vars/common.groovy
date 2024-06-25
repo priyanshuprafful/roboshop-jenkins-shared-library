7c1914f2ad90a2f55743ba4fd003846549ceffb2
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-25 05:48:54 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -47,17 +47,19 @@ def prepareArtifacts() {
 }
 def artifactUpload () {
 
-    env.NEXUS_USER = sh ( script: "aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
-    env.NEXUS_PASS = sh ( script: "aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
-    wrap([$class: 'MaskPasswordsBuildWrapper',
-          varPasswordPairs: [[password: NEXUS_PASS] , [password:NEXUS_USER]]]) {
-
-        //   if (app_language == "node_js" || app_language == "angular") {
-        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-        // }
-
-    }
- //   if (app_language == "node_js" || app_language == "angular") {
-        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
- // }
+//    env.NEXUS_USER = sh ( script: "aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
+//    env.NEXUS_PASS = sh ( script: "aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs" , returnStdout: true ).trim()
+//    wrap([$class: 'MaskPasswordsBuildWrapper',
+//          varPasswordPairs: [[password: NEXUS_PASS] , [password:NEXUS_USER]]]) {
+//
+//        //   if (app_language == "node_js" || app_language == "angular") {
+//        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//        // }
+//
+//    }
+// //   if (app_language == "node_js" || app_language == "angular") {
+//        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+// // }
+    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 529080510656.dkr.ecr.us-east-1.amazonaws.com'
+    sh 'docker push 529080510656.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
 }
