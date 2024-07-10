5aac987e4a8828404c5cf208578997d0c0c8061a
priyanshuprafful <priyanshuprafful@gmail.com>
2024-07-10 23:36:21 +0530

    updating ./roboshop-jenkins-shared-library

---
+++ vars/helm.groovy
@@ -0,0 +1,54 @@
+def call() {
+    pipeline {
+        agent any
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            string(name: 'app_version', defaultValue: '' , description: 'App_Version')
+            string(name: 'component', defaultValue: '' , description: 'Component')
+            string(name: 'environment', defaultValue: '' , description: 'Environment')
+
+
+        }
+
+        stages {
+
+            stage('Clone Application') {
+                steps {
+                    dir('APP') {
+                        git branch: 'main', url: "https://github.com/priyanshuprafful/${component}"
+                    }
+
+
+                }
+            }
+
+
+
+            stage('Deploy Helm Charts') {
+                steps {
+                    script {
+                        sh 'helm install ${component} APP/helm/${environment}.yaml'
+
+
+
+                        }
+
+                    }
+
+
+
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
